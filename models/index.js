28f01f43424e714adcb8e77c83e74c4e94728931
Chris Stevenson <chrislstevenson@gmail.com>
2024-02-21 09:10:05 -0500

    Create models and associations

--- models/index.js
+++ models/index.js
@@ -4,13 +4,21 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongsTo Category, this is a one-to-many relationship
+// A product can be in only one category, but a category can have many products
+// There is a FK (category_id) in the Product model; Category has no FKs
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product)
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, { through: ProductTag });
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: ProductTag });
 
 module.exports = {
   Product,
