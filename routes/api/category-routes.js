038dd12da8e11c0baf922b5fca192cda193dee7b
Chris Stevenson <chrislstevenson@gmail.com>
2024-02-21 10:04:13 -0500

    Create Category POST, PUT, DELETE routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -43,16 +43,46 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const newCategory = await Category.create({
+      category_name: req.body.category_name
+    });
+    res.status(200).json(newCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    updatedCategory = await Category.update(
+      {
+        category_name: req.body.category_name
+      },
+      { where: { id: req.params.id } }
+    );
+    if (updatedCategory[0] !== 0) {
+      res.status(200).json(`Name of category ${req.params.id} updated to ${req.body.category_name}.`);
+    } else {
+      res.status(200).json('No such category exists to update');
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const deletedCategory = await Category.destroy({ where: { id: req.params.id } });
+    if (deletedCategory) res.status(200).json(`Deleted category ${req.params.id}`);
+    else res.status(200).json("A category with that ID doesn't exist.");
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
